cf5a25fb09a3752818b6bb5a1459a5ce2590effa
Jamon Dixon <jamonadixon@gmail.com>
2021-02-05 18:54:50 -0700

    commit tree 7ef77828935e874f09f379c98b7afa00c9753b33

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Company
     attr_accessor :name, :size
 
@@ -13,4 +14,27 @@ companies = [
     Company.new('Delta', 3000)
 ]
 
-### Your code below this
+# Generate a list of strings with the name and size (eg. "Alpha - 30")
+# * Combine all the sizes with reduce
+# * Filter the list to show only companies over 100
+# * Find the company named "Beta"
+# * Find the largest company
+# * Sort the companies from largest to smallest
+def name_and_size_list
+    all_companies = []
+    companies.each do |new_company|
+    all_companies << "#{new_companies.name} - #{new_companies.size}"
+    all_companies
+end
+
+
+
+def companies_over_100 
+    companies.filter do |new_company|
+    companies_over_100 = {}
+    if new_company.size >= 100
+        companies_over_100 << new_company.name
+    end
+end
+
+puts companies_over_100
